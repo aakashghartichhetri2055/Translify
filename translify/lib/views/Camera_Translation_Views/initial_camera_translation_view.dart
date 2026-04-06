4d355931576c70cdb8813dcabcbecd5b200630e7
Kelvin Capriel Reyes <kelvincapriel@gmail.com>
2026-04-06 16:26:04 -0400

    Added language selectors and capture button

--- translify/lib/views/Camera_Translation_Views/initial_camera_translation_view.dart
+++ translify/lib/views/Camera_Translation_Views/initial_camera_translation_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:translify/languages/languages.dart';
 import 'package:translify/colors/colors.dart';
+import 'package:translify/widgets/language_selector.dart';
 import 'package:camera/camera.dart';
 
 class InitialCameraTranslationView extends StatefulWidget {
@@ -24,12 +25,6 @@ class _InitialCameraTranslationViewState
 
   // The future to control the widget building based on status of controller
   Future<void>? initializeControllerFuture;
-
-  @override
-  void initState() {
-    initializeCameras();
-    super.initState();
-  }
 
   void initializeCameras() async {
     // Get cameras
@@ -60,6 +55,60 @@ class _InitialCameraTranslationViewState
     });
   }
 
+  // Target Language Variables
+  String targetLanguage = "en";
+  String targetLanguageButtonMessage = "Current Language: English";
+  void updateTargetLanguage(Languages language) {
+    // If the languages for the first and second speaker match, swap them around
+    // Ex: First person has English, and Second person has Spanish. If the First person picks Spanish in their menu, First person will have Spanish and Second person will have English
+    if (sourceLanguage == language.code) {
+      setState(() {
+        sourceLanguage = targetLanguage;
+        sourceLanguageButtonMessage = targetLanguageButtonMessage;
+
+        targetLanguage = language.code;
+        targetLanguageButtonMessage = "Current Language: ${language.name}";
+      });
+    }
+    // Else only update the first person
+    else {
+      setState(() {
+        targetLanguage = language.code;
+        targetLanguageButtonMessage = "Current Language: ${language.name}";
+      });
+    }
+  }
+
+  // Source Language Variables
+  String sourceLanguage = "es";
+  String sourceLanguageButtonMessage = "Current Language: Spanish";
+  void updateSourceLanguage(Languages language) {
+    // If the languages for the first and second speaker match, swap them around
+    // Ex: First person has English, and Second person has Spanish. If the First person picks Spanish in their menu, First person will have Spanish and Second person will have English
+    if (targetLanguage == language.code) {
+      setState(() {
+        targetLanguage = sourceLanguage;
+        targetLanguageButtonMessage = sourceLanguageButtonMessage;
+
+        sourceLanguage = language.code;
+        sourceLanguageButtonMessage = "Current Language: ${language.name}";
+      });
+    }
+    // Else only update the first person
+    else {
+      setState(() {
+        sourceLanguage = language.code;
+        sourceLanguageButtonMessage = "Current Language: ${language.name}";
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeCameras();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,7 +124,79 @@ class _InitialCameraTranslationViewState
               future: initializeControllerFuture,
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.done) {
-                  return CameraPreview(currentCamera);
+                  return Column(
+                    children: [
+                      // The camera view
+                      CameraPreview(currentCamera),
+
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * .05,
+                      ),
+
+                      // The button to select the target language
+                      LanguageSelector(
+                        updateLanguageChoice: (langauge) => {
+                          updateTargetLanguage(langauge),
+                        },
+                        buttonText: targetLanguageButtonMessage,
+                        buttonBackgroundColor:
+                            TranslifyColors.cameraTranslationAccentColor,
+                        buttonTextColor: TranslifyColors.darkButtonText,
+                        menuItemBackgroundColor:
+                            TranslifyColors.nonAdminButtonColor,
+                        menuItemTextColor:
+                            TranslifyColors.cameraTranslationAccentColor,
+                        currentLanguage: targetLanguage,
+                        disabledColor: TranslifyColors.disabledOptionColor,
+                      ),
+
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * .025,
+                      ),
+
+                      // The button to select the source language
+                      LanguageSelector(
+                        updateLanguageChoice: (langauge) => {
+                          updateSourceLanguage(langauge),
+                        },
+                        buttonText: sourceLanguageButtonMessage,
+                        buttonBackgroundColor:
+                            TranslifyColors.cameraTranslationAccentColor,
+                        buttonTextColor: TranslifyColors.darkButtonText,
+                        menuItemBackgroundColor:
+                            TranslifyColors.nonAdminButtonColor,
+                        menuItemTextColor:
+                            TranslifyColors.cameraTranslationAccentColor,
+                        currentLanguage: sourceLanguage,
+                        disabledColor: TranslifyColors.disabledOptionColor,
+                      ),
+
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * .05,
+                      ),
+
+                      // The button to take a picture
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: TranslifyColors.cameraTranslationAccentColor,
+                            width: 2,
+                          ),
+                        ),
+
+                        child: IconButton(
+                          onPressed: () => {},
+                          icon: Icon(Icons.radio_button_checked, size: 50),
+                          style: IconButton.styleFrom(
+                            backgroundColor: TranslifyColors.backgroundColor,
+                            foregroundColor:
+                                TranslifyColors.cameraTranslationAccentColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
                 } else {
                   return Center(
                     child: CircularProgressIndicator(
