e4d7a7c85de8bec986b995454b0f3c5b7077da5b
Rusha Limbu <156543938+rkedem@users.noreply.github.com>
2026-04-07 14:05:20 -0400

    Create speech_demo.py
    Add speech processing component using Faster-Whisper

---
+++ speechProcessing/speech_demo.py
@@ -0,0 +1,52 @@
+from faster_whisper import WhisperModel
+import sounddevice as sd
+import numpy as np
+import wave
+import os
+
+
+class SpeechProcessor:
+    def __init__(self):
+        print("Welcome to Translify")
+        self.model = WhisperModel("base", device="cpu", compute_type="int8")
+    
+
+    def record_audio(self, filename="speech.wav"):
+        samplerate = 16000
+        duration = 5
+
+        print("Listening... Speak now.")
+        audio = sd.rec(
+            int(duration * samplerate),
+            samplerate=samplerate,
+            channels=1,
+            dtype="int16"
+        )
+
+        sd.wait()
+
+    
+        with wave.open(filename, "wb") as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)
+            wf.setframerate(samplerate)
+            wf.writeframes(audio.tobytes())
+
+        return filename
+
+    def transcribe(self):
+        audio_file = self.record_audio()
+
+        segments, _ = self.model.transcribe(audio_file)
+
+        text = ""
+        for segment in segments:
+            text += segment.text + " "
+
+        return text.strip()
+
+
+if __name__ == "__main__":
+    speech = SpeechProcessor()
+    result = speech.transcribe()
+    print("You said:", result)
