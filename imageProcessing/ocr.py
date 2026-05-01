044ed4c8d32ed1b79a678bd05ed68d8fc935beff
Kelvin Capriel Reyes <kelvincapriel@gmail.com>
2026-05-01 18:17:11 -0400

    Prevent image to text from listening on all ports

--- imageProcessing/ocr.py
+++ imageProcessing/ocr.py
@@ -172,4 +172,5 @@ def captured_image():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+   #uvicorn.run(app, host="0.0.0.0", port=8001)
+   uvicorn.run(app, host="127.0.0.1", port=8001)
