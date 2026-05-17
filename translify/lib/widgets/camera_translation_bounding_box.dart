8472d3402c1dac3644ed064cebfd304e9f51167a
Kelvin Capriel Reyes <kelvincapriel@gmail.com>
2026-05-17 15:54:28 -0400

    Added way to see a bounding box in the camera translation for more details on a particular section of text

--- translify/lib/widgets/camera_translation_bounding_box.dart
+++ translify/lib/widgets/camera_translation_bounding_box.dart
@@ -1,7 +1,7 @@
 // One of the boxes for detected text in Camera Translation
 
 import 'package:flutter/material.dart';
-import 'package:translify/widgets/button.dart';
+import 'package:translify/colors/colors.dart';
 
 class CameraTranslationBoundingBox extends StatefulWidget {
   final double x;
@@ -30,7 +30,7 @@ class CameraTranslationBoundingBox extends StatefulWidget {
 class _CameraTranslationBoundingBoxState
     extends State<CameraTranslationBoundingBox> {
   // Define the current colors
-  Color buttonBackgroundColor = Colors.red;
+  Color buttonBackgroundColor = const Color.fromARGB(196, 244, 67, 54);
   Color textColor = Colors.black;
 
   @override
@@ -43,9 +43,78 @@ class _CameraTranslationBoundingBoxState
         height: widget.height,
         child: Center(
           child: FilledButton(
-            onPressed: () => print(
-              "Original: ${widget.originalText}. Translation: ${widget.translatedText}",
-            ),
+            onPressed: () {
+              // Show a dialog with the original text and translated text
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    backgroundColor: TranslifyColors.backgroundColor,
+
+                    title: Text(
+                      "Translation Details",
+                      style: TextStyle(color: TranslifyColors.headerTextColor),
+                    ),
+
+                    content: SingleChildScrollView(
+                      child: ListBody(
+                        children: [
+                          // Original text stuff
+                          Text(
+                            "Original:",
+                            style: TextStyle(
+                              color:
+                                  TranslifyColors.cameraTranslationAccentColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          Text(
+                            widget.originalText,
+                            style: TextStyle(
+                              color: TranslifyColors.headerTextColor,
+                            ),
+                          ),
+
+                          SizedBox(height: 12),
+
+                          // Translated text
+                          Text(
+                            "Translation:",
+                            style: TextStyle(
+                              color:
+                                  TranslifyColors.cameraTranslationAccentColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          Text(
+                            widget.translatedText,
+                            style: TextStyle(
+                              color: TranslifyColors.headerTextColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Button to close the popup
+                    actions: [
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              TranslifyColors.cameraTranslationAccentColor,
+                          foregroundColor: TranslifyColors.darkButtonText,
+                        ),
+                        child: const Text("Close"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
             style: ElevatedButton.styleFrom(
               backgroundColor: buttonBackgroundColor,
               overlayColor: null,
